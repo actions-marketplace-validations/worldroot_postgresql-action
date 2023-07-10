7f30c67f38539890458c43c5e8561ecf4e10c153
Mehdi Behira <mehdibehira@gmail.com>
2023-07-10 10:31:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
 docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-docker_run="$docker_run -e echo max_locks_per_transaction = 1024 | sudo tee -a /etc/postgresql/10/main/postgresql.conf " 
+docker_run="$docker_run echo max_locks_per_transaction = 1024 | sudo tee -a /etc/postgresql/10/main/postgresql.conf " 
 docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
 sh -c "$docker_run"
